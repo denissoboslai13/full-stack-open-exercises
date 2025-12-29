5ad0a81e0f4bf5e233358b8974770ed4008c43be
denissoboslai13 <soboslaid@gmail.com>
2025-12-29 11:45:17 +0100

    fully working phonebook as of the end of part3

---
+++ part3-phonebook-all/backend/models/person.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery',false)
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url, { family: 4 })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: value => /^\d{2,3}-\d+$/.test(value),
+      message: 'Number must be of format 123-45678'
+    },
+    required: true
+  },
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
