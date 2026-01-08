5d9beeb8e5fe5d0c53df2662089aaf83aeed12a8
denissoboslai13 <soboslaid@gmail.com>
2026-01-08 13:53:11 +0100

    commit for part 4 blog list exercise

---
+++ part4-bloglist/backend/controllers/blogs.js
@@ -0,0 +1,92 @@
+const jwt = require('jsonwebtoken')
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const { tokenExtractor, userExtractor } = require('../utils/middleware');
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
+  response.json(blogs)
+})
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+})
+
+blogsRouter.post('/', tokenExtractor, userExtractor, async (request, response) => {
+  const body = request.body
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = request.user
+
+  if (!user) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
+
+  if (!body.title || !body.url) {
+    return response.status(400).json({
+      error: 'title or url missing'
+    })
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: user.name,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', tokenExtractor, userExtractor, async (request, response) => {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
+  if (blog.user.toString() === user.id.toString()){
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } else {
+      return response.status(401).json({ error: 'token invalid' })
+  }
+})
+
+blogsRouter.put('/:id', tokenExtractor, userExtractor, async (request, response, next) => {
+  const { title, url, likes } = request.body
+  
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = request.user
+
+  const blog = await Blog.findById(request.params.id)
+  if (blog.user.toString() === user.id.toString()){
+
+      blog.title = title
+      blog.author = user.name
+      blog.url = url
+      blog.likes = likes
+
+      const updatedBlog = await blog.save()
+      response.status(200).json(updatedBlog)
+  } else {
+      return response.status(401).json({ error: 'token invalid' })
+  }
+})
+
+module.exports = blogsRouter
---
+++ part4-bloglist/backend/utils/list_helper.js
@@ -0,0 +1,41 @@
+const _ = require('lodash');
+
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+    const sum = blogs.map(blog => (blog.likes))
+    return sum.length === 1 ? blogs[0].likes : sum.reduce((partialSum, a) => partialSum + a, 0);
+}
+
+const favoriteBlog = (blogs) => {
+    const likes = blogs.map(blog => (blog.likes))
+    const highest = Math.max(...likes)
+    const found = blogs.find(blog => blog.likes === highest)
+    return blogs.length === 1 ? blogs[0] : found
+}
+
+const mostBlogs = (blogs) => {
+    const highest = _.map(_.groupBy(blogs, 'author'), (blogs, author) => ({
+        author,
+        blogs: blogs.length,
+    }))
+    return _.maxBy(highest, 'blogs')
+}
+
+const mostLikes = (blogs) => {
+    const result = _.map(_.groupBy(blogs, 'author'), (blogs, author) => ({
+        author,
+        likes: _.sumBy(blogs, 'likes'),
+    }))
+    return _.maxBy(result, 'likes')
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
+}
